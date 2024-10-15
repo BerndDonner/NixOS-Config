e8b3fce27521d8cdddca2eded697eb5ca50588d3
Bernd Donner <bernd.donner@sabel.com>
2024-10-15 07:57:10 +0200

    US keyboard layout

--- configuration.nix
+++ configuration.nix
@@ -30,7 +30,7 @@
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
-    keyMap = "eu";
+    keyMap = "us";
     # useXkbConfig = true; # use xkb.options in tty.
   };
 
@@ -47,7 +47,7 @@
 
   # Configure keymap in X11
   services.xserver.xkb = {
-    layout = "eu";
+    layout = "us";
     variant = "nodeadkeys";
   };
 
