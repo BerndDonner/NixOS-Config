5dc41fc85561200c275410f558b05ad8416d6119
Bernd Donner <bernd.donner@sabel.com>
2024-11-11 08:11:46 +0100

    forgotton config change in last commit

--- configuration.nix
+++ configuration.nix
@@ -122,28 +122,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # Disable suspend and hibernate. It crashes nvidia 555 driver.
-#  systemd = {
-#    targets = {
-#      sleep = {
-#        enable = false;
-#        unitConfig.DefaultDependencies = "no";
-#      };
-#      suspend = {
-#        enable = false;
-#        unitConfig.DefaultDependencies = "no";
-#      };
-#      hibernate = {
-#        enable = false;
-#        unitConfig.DefaultDependencies = "no";
-#      };
-#      "hybrid-sleep" = {
-#        enable = false;
-#        unitConfig.DefaultDependencies = "no";
-#      };
-#    };
-#  };
-
   # Enable OpenGL
   hardware.opengl = {
     enable = true;
@@ -155,11 +133,39 @@
   services.xserver.videoDrivers = ["nvidia"];
 
   # all this crap does not help with suspend!
-  boot.kernelParams = [ "nvidia_drm.fbdev=1" "nvidia-drm.modeset=1" "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
+  boot = {
+    kernelParams = [ "nvidia_drm.fbdev=1" ];
+    
+    extraModprobeConfig =
+      "options nvidia "
+      + lib.concatStringsSep " " [
+        # nvidia assume that by default your CPU does not support PAT,
+        # but this is effectively never the case in 2023
+        "NVreg_UsePageAttributeTable=1"
+        # This is sometimes needed for ddc/ci support, see
+        # https://www.ddcutil.com/nvidia/
+        #
+        # Current monitor does not support it, but this is useful for
+        # the future
+        "NVreg_RegistryDwords=RMUseSwI2c=0x01;RMI2cSpeed=100"
+      ];
+  };
+
+  environment.variables = {
+    # Required to run the correct GBM backend for nvidia GPUs on wayland
+    GBM_BACKEND = "nvidia-drm";
+    # Apparently, without this nouveau may attempt to be used instead
+    # (despite it being blacklisted)
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    # Hardware cursors are currently broken on nvidia
+    WLR_NO_HARDWARE_CURSORS = "1";
+  };
+
+  imports = [ ./vaapi.nix ];
 
   hardware.nvidia = {
     # does not help with suspend!
-    forceFullCompositionPipeline = true;
+    #forceFullCompositionPipeline = true;
 
     # Modesetting is required.
     modesetting.enable = true;
@@ -168,11 +174,11 @@
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
     # of just the bare essentials.
-    powerManagement.enable = false;
+    powerManagement.enable = true;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+    #powerManagement.finegrained = false;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
@@ -181,24 +187,30 @@
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
+    open = true;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
+    # The nvidia-settings build is currently broken due to a missing
+    # vulkan header; re-enable whenever
+    # 0384602eac8bc57add3227688ec242667df3ffe3the hits stable.
+    nvidiaSettings = false;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
 
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "555.58.02";
-      sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
-      sha256_aarch64 = "sha256-wb20isMrRg8PeQBU96lWJzBMkjfySAUaqt4EgZnhyF8=";
-      openSha256 = "sha256-8hyRiGB+m2hL3c9MDA/Pon+Xl6E788MZ50WrrAGUVuY=";
-      settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
-      persistencedSha256 = "sha256-a1D7ZZmcKFWfPjjH1REqPM5j/YLWKnbkP9qfRyIyxAw=";
+      version = "560.35.03";
+      sha256_64bit = "sha256-8pMskvrdQ8WyNBvkU/xPc/CtcYXCa7ekP73oGuKfH+M=";
+      sha256_aarch64 = "sha256-s8ZAVKvRNXpjxRYqM3E5oss5FdqW+tv1qQC2pDjfG+s=";
+      openSha256 = "sha256-/32Zf0dKrofTmPZ3Ratw4vDM7B+OgpC4p7s+RHUjCrg=";
+      settingsSha256 = "sha256-kQsvDgnxis9ANFmwIwB7HX5MkIAcpEEAHc8IBOLdXvk=";
+      persistencedSha256 = "sha256-E2J2wYYyRu7Kc3MMZz/8ZIemcZg68rkzvqEwFAL3fFs=";
     };
 
-    #package = config.boot.kernelPackages.nvidiaPackages.stable;
+    vaapi = {
+      enable = true;
+      firefox.enable = true;
+    };
   };
 
   # Copy the NixOS configuration file and link it from the resulting system
