22931eec7c1b4ddc039941db2fb4335b7cc8bb8b
Bernd Donner <bernd.donner@sabel.com>
2026-09-12 12:33:10 +0200

    updated zk config

--- modules/home-manager/zk.nix
+++ modules/home-manager/zk.nix
@@ -1,7 +1,23 @@
 { ... }:
 
 {
-  programs.zk = {
-    enable = true;
+  programs = {
+    zk.enable = true;
+
+    # Required for `zk edit --interactive`
+    fzf.enable = true;
+
+    # Pretty preview of Markdown notes inside fzf
+    bat.enable = true;
+
+    bash.initExtra = ''
+      zk() {
+        if [ "$#" -eq 0 ]; then
+          command zk edit --interactive
+        else
+          command zk "$@"
+        fi
+      }
+    '';
   };
 }
